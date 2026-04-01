935e9cb1822bcd50669842890535f4bb3f5f8805
nobody <nobody@nowhere.net>
2026-04-01 10:30:04 -0600

    Set up demo to match current `modmad` app.

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui::{CentralPanel, Context, Response, Ui, ViewportBuilder, Widget};
+use eframe::{Frame, NativeOptions, run_native};
+use hexohexes::Board;
+use hexohexes_egui::BoardFrame;
+
+fn main() -> eframe::Result<()> {
+    let app = App {
+        board: Board::new_defaults(3),
+    };
+
+    run_native(
+        env!("CARGO_PKG_NAME"),
+        NativeOptions {
+            viewport: ViewportBuilder::default().with_maximized(true),
+            persist_window: false,
+            ..Default::default()
+        },
+        Box::new(|_cc| Ok(Box::new(app))),
+    )
+}
+
+struct App {
+    board: Board<()>,
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| ui.add(self));
+    }
+}
+
+impl Widget for &mut App {
+    fn ui(self, ui: &mut Ui) -> Response {
+        ui.add(BoardFrame::new(&self.board))
+    }
 }
